0b17b40ea8c32cec7578444e135d4ecbf8d017d4
deeptibhangale <deeptibhangale@gmail.com>
2024-03-16 07:53:47 -0700

    done all changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Professionals from "./components/Professionals";
+import Clients from "./components/Clients";
+
 function App() {
   return (
     <div className="bg-stone-600 ">
       <h2 className="bg-pink-400 text-gray-500 text-center text-xl">
         Welcome to Cleaning Services
       </h2>
-      <menu>
-        <div className="text-lg">
-          <Professionals />
-        </div>
-        <p>users</p>
-      </menu>
+
+      <div className="text-lg">
+        <Professionals />
+      </div>
+      <Clients />
     </div>
   );
 }
---
+++ src/components/Clients.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export default function Clients() {
+  const [data, setData] = useState([]);
+  function handleClick() {
+    fetch("http://localhost:8080/workers", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data), console.log(data);
+      })
+      .catch("not fetching data");
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>Client-Detail</button>
+      <div>
+        <h1> client details are given below</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Id </th>
+              <th>Name </th>
+              <th>Title </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.title}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
